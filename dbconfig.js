800976386169415ae8eeba8e5f9c57a3d2ef4ab0
gustavogaspar <gaspar.gualves@gmail.com>
2019-07-10 17:23:36 -0300

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,6 @@
+    module.exports = {
+        user          : "admin",
+        password      : "T.vinho161421",
+        connectString : "adb.us-phoenix-1.oraclecloud.com:1522/lt4sf9k8rcnyncw_testetp_high.atp.oraclecloud.com",
+        externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
+      };
