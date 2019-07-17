4db042904b64e9ee2a819e2db13f6aa4d7387cb4
gustavogaspar <gaspar.gualves@gmail.com>
2019-07-17 00:21:57 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 function btnCall(chamada) {
     document.getElementById('status').innerText = "PROCESSANDO...";
-    axios.get('http://localhost:3000/' + chamada)
+    axios.get('http://<IP_PUBLICO>:3000/' + chamada)
         .then(response => {
             document.getElementById('status').innerText = response.data
             return console.log(response.data)
